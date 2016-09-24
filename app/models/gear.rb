115f598c4111388e7922a5b8a384651d771760fd
yukiya <yukiya.ueda@gmail.com>
2016-09-25 01:14:44 +0900

    i18n

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -1,3 +1,7 @@
 class Gear < ApplicationRecord
   belongs_to :category
+
+  validates :title, presence: true
+  validates :name, presence: true
+  validates :weight, presence: true, numericality: true
 end
