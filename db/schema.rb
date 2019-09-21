99b55ec7b908faededf6223ec0ac3e512688ba09
yukiya <yukiya.ueda@gmail.com>
2019-09-21 12:39:52 +0900

    rails 6

--- db/schema.rb
+++ db/schema.rb
@@ -2,81 +2,81 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023051740) do
+ActiveRecord::Schema.define(version: 2019_09_21_033815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
+  create_table "categories", id: :serial, force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "gears", force: :cascade do |t|
-    t.integer  "category_id"
-    t.string   "title"
-    t.string   "name"
-    t.text     "detail"
-    t.integer  "weight"
-    t.text     "comment"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "image"
-    t.integer  "user_id"
-    t.index ["category_id"], name: "index_gears_on_category_id", using: :btree
-    t.index ["user_id"], name: "index_gears_on_user_id", using: :btree
+  create_table "gears", id: :serial, force: :cascade do |t|
+    t.integer "category_id"
+    t.string "title"
+    t.string "name"
+    t.text "detail"
+    t.integer "weight"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image"
+    t.integer "user_id"
+    t.index ["category_id"], name: "index_gears_on_category_id"
+    t.index ["user_id"], name: "index_gears_on_user_id"
   end
 
-  create_table "packing_list_gears", force: :cascade do |t|
-    t.integer  "packing_list_id"
-    t.integer  "gear_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["gear_id"], name: "index_packing_list_gears_on_gear_id", using: :btree
-    t.index ["packing_list_id"], name: "index_packing_list_gears_on_packing_list_id", using: :btree
+  create_table "packing_list_gears", id: :serial, force: :cascade do |t|
+    t.integer "packing_list_id"
+    t.integer "gear_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["gear_id"], name: "index_packing_list_gears_on_gear_id"
+    t.index ["packing_list_id"], name: "index_packing_list_gears_on_packing_list_id"
   end
 
-  create_table "packing_lists", force: :cascade do |t|
-    t.string   "title"
-    t.text     "comment"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.integer  "user_id"
-    t.boolean  "public",     default: false
-    t.string   "image"
-    t.index ["user_id"], name: "index_packing_lists_on_user_id", using: :btree
+  create_table "packing_lists", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.boolean "public", default: false
+    t.string "image"
+    t.index ["user_id"], name: "index_packing_lists_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "name"
-    t.string   "confirmation_token"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "role"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.string "unconfirmed_email"
+    t.integer "role"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "gears", "categories"
